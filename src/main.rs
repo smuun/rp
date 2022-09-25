a0c6593da71dfd50926b64ce490aaceab58f963b
smuun <72632230+smuun@users.noreply.github.com>
2022-09-25 15:28:10 -0400

    wip process input

--- src/main.rs
+++ src/main.rs
@@ -7,55 +7,56 @@ enum LineType {
     Minus,
     Times,
     Divide,
-    Number
+    Number,
+    Invalid,
 }
 
 struct Line {
     t: LineType,
-    v: f64
+    v: f64,
 }
 
-fn try_operator(val) -> Option<Line> {
-
+fn try_operator(value: &str) -> Option<Line> {
+    let t: Option<LineType> = match value {
+        "+" => Some(LineType::Plus),
+        _ => None,
+    };
+    match t {
+        return 
+    }
+    return Some(Line { t: t, v: 0.00 });
 }
-Some(Line {t: match val {
-                "+"
-                default 
 
-            }
-                , v: 0.00})
-        }
-
-
-fn process(buff: String)-> Option<Line> {
+fn process(buff: String) -> Option<Line> {
     let parsed = buff.parse::<f64>();
     return match parsed {
-        Ok(val) => Some(Line {t: LineType::Number, v: val}),
+        Ok(val) => Some(Line {
+            t: LineType::Number,
+            v: val,
+        }),
         Err(val) => match buff.parse::<char>() {
             Ok(val) => try_operator(val),
-            Err(val) => None()
-        }
-    }
+            Err(val) => None(),
+        },
+    };
 }
 
 fn main() {
     println!("rp: command line rpn calculator");
-    println!("USAGE:
+    println!(
+        "USAGE:
         <number>: add to stack,
         <operator> (+, -, *, /): pop top two elements, operate, and push
         p: print top of stack
         f: print whole stack
         q or <C-d>: quit
-        ");
+        "
+    );
     let mut buff = String::new();
     let mut stack: Vec<f64> = Vec::new();
     loop {
         buff.clear();
-        io::stdin()
-            .read_line(&mut buff)
-            .expect("readline failed");
+        io::stdin().read_line(&mut buff).expect("readline failed");
         let val = process(buff);
-        
-
     }
 }
