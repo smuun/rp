42cb5d71b8a6aaa38633e3c47ad79064ae858031
smuun <72632230+smuun@users.noreply.github.com>
2022-10-24 14:25:44 -0400

    All arithmetic operations working
    TODO: error handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io;
 use std::io::Write;
+use std::env;
+use colored::Colorize;
 // use rug::{Float, Assign};
 
 #[allow(dead_code)]
@@ -9,15 +11,20 @@ enum LineType {
     Minus,
     Times,
     Divide,
+    Pow,
     //entry
     Number,
+    Negate,
     //stack ops
     Pop,
+    Clear,
     //viewing
     Print,
     Full,
-    Clear,
+    ClearScreen,
     Quit,
+    Swap,
+    Flip
 }
 
 struct Line {
@@ -31,13 +38,29 @@ fn try_operator(value: &char) -> Option<LineType> {
         '-' => Some(LineType::Minus),
         '*' => Some(LineType::Times),
         '/' => Some(LineType::Divide),
+        '_' => Some(LineType::Negate),
+        '^' => Some(LineType::Pow),
         'd' => Some(LineType::Pop),
         'p' => Some(LineType::Print),
         'f' => Some(LineType::Full),
         'c' => Some(LineType::Clear),
         'q' => Some(LineType::Quit),
+        's' => Some(LineType::Swap),
+        'S' => Some(LineType::Flip),
+        ' ' => Some(LineType::ClearScreen),
         _ => {
-            //println!("Failed to parse operator");
+            return None;
+        }
+    };
+    return t;
+}
+
+fn try_command(value: &str) -> Option<LineType> {
+    let t: Option<LineType> = match value {
+        "cl" => Some(LineType::ClearScreen),
+        "quit" => Some(LineType::Quit),
+        "exit" => Some(LineType::Quit),
+        _ => {
             return None;
         }
     };
@@ -53,31 +76,56 @@ fn process(buff: &String) -> Option<Line> {
             v: val,
         }),
         Err(_) => {
-            //println!("Failed to parse float: buffer contains {}", *buff);
             return match buff.trim().parse::<char>() {
                 Ok(val) => match try_operator(&val) {
                     Some(t) => Some(Line { t: t, v: 0.0 }),
                     _ => None,
                 },
-                Err(_) => None,
+                Err(_) => match buff.trim().parse::<String>() {
+                    Ok(val) => match try_command(&val) {
+                        Some(t) => Some(Line { t: t, v: 0.0 }),
+                        _ => None,
+                    }
+                    Err(_) => None,
+                },
             };
         }
     };
 }
 
+fn poptwo(v: &mut Vec<f64>) -> Option<(f64, f64)> {
+    let a = v.pop().unwrap();
+    let b = v.pop().unwrap();
+    return Some((a, b))
+}
+
+fn printresult(r: &f64) {
+    println!("{}", r.to_string().bold());
+    println!("");
+}
+
 fn main() {
     println!("rp: command line rpn calculator");
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1{
     println!(
-        "USAGE:
-        <number>: add to stack,
-        <operator> (+, -, *, /): pop top two elements, operate, and push
-        p: print top of stack
-        f: print stack
-        d: drop (pop) element
-        c: clear stack
-        q or <C-d>: quit
+        "USAGE:  rp
+        COMMANDS:
+        <number>:                        add to stack
+        <operator> (+, -, *, /, ^):      operate on last two elements
+        _                                negate last element
+        clear, cl:                       clear screen
+        p:                               print top of stack
+        f:                               print whole stack
+        d:                               drop last element
+        c:                               clear stack
+        s:                               flip last two elements
+        S:                               flip stack
+        q, quit, exit, <C-c>:            quit
         "
     );
+    std::process::exit(1)
+    }
     let mut buff = String::new();
     let mut stack: Vec<f64> = Vec::new();
     loop {
@@ -89,27 +137,60 @@ fn main() {
         match val {
             Some(i) => match i.t {
                 LineType::Number => stack.push(i.v),
-                LineType::Print => println!("{}", stack.last().unwrap()),
+                LineType::Print => println!("{}", &stack.last().unwrap()),
                 LineType::Full => {
                     for s in &stack {
-                        println!("{}", s);
+                        print!("{}\n", s);
                     }
+                    print!("\n");
+                    io::stdout().flush().unwrap();
                 }
-                LineType::Pop => println!("{}", stack.pop().unwrap()),
-                LineType::Plus => println!("plus"),
+                LineType::Pop => printresult(&(stack.pop().unwrap())),
+                LineType::Swap => {
+                    let (a, b) = poptwo(&mut stack).unwrap();
+                    stack.push(a);
+                    stack.push(b);
+                },
+                LineType::Flip => {stack.reverse()},
                 LineType::Clear => stack = Vec::new(),
+                LineType::ClearScreen => print!("{esc}c", esc = 27 as char),
                 LineType::Plus => {
-                    let a = stack.last().unwrap();
-                    let b = stack.last().unwrap();
+                    let (a, b) = poptwo(&mut stack).unwrap();
                     let r = a + b;
-                    println!("{}", r);
+                    printresult(&r);
+                    stack.push(r);
+                }
+                LineType::Minus => {
+                    let (a, b) = poptwo(&mut stack).unwrap();
+                    let r = a - b;
+                    printresult(&r);
+                    stack.push(r);
+                }
+                LineType::Times => {
+                    let (a, b) = poptwo(&mut stack).unwrap();
+                    let r = a * b;
+                    printresult(&r);
+                    stack.push(r);
+                }
+                LineType::Divide => {
+                    let (a, b) = poptwo(&mut stack).unwrap();
+                    let r = b / a;
+                    printresult(&r);
+                    stack.push(r);
+                }
+                LineType::Pow => {
+                    let (a, b) = poptwo(&mut stack).unwrap();
+                    let r = f64::powf(b, a);
+                    printresult(&r);
+                    stack.push(r);
+                }
+                LineType::Negate => {
+                    let a = stack.pop().unwrap();
+                    let r = -a;
+                    printresult(&r);
                     stack.push(r);
                 }
                 LineType::Quit => std::process::exit(0),
-                _ => {
-                    println!("?");
-                    continue;
-                }
             },
             None => {
                 println!("?");
