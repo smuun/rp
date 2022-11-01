f1621f184d79219f16295a9cfbe96fc98f1a21eb
smuun <72632230+smuun@users.noreply.github.com>
2022-11-01 15:15:44 -0400

    Install script

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-cargo build --release && \
-	cp ./target/release/rp ~/.local/bin/rp && \
-	chmod +x ~/.local/bin/rp && \
-	echo Done.
+echo "Build rp and copy binary to ~/.local/bin/rp.  Proceed?"
+select yn in "Yes" "No"; do
+  case $yn in
+    Yes) break;;
+    No ) exit;;
+  esac
+done
+set -e
+mkdir -p ~/.local/bin
+cargo build --release 
+cp ./target/release/rp ~/.local/bin/rp
+chmod +x ~/.local/bin/rp 
+echo Done.  Make sure ~/.local/bin is in your \$PATH.
